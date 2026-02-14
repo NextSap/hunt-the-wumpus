0be01d4950d0de0160c18e3b5a8871a1ff2816ed
NextSap <nextsap@outlook.com>
2026-02-14 10:59:11 +0100

    implement login, register and logout routes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,11 @@
 import os
 
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
+from markupsafe import escape
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import db_connect
 
 
 def create_app():
@@ -10,16 +15,92 @@ def create_app():
     # ensure the instance folder exists
     os.makedirs(app.instance_path, exist_ok=True)
 
-    @app.route('/')
+    @app.route("/")
     def index():
         return redirect("/login")
 
-    @app.route('/login')
+    @app.route("/login", methods=["GET", "POST"])
     def login():
-        return render_template('auth/login.html')
+        if session.get("username", -1) != -1:
+            return redirect("/game")
 
-    @app.route('/register')
+        if request.method == "POST":
+            username_input = escape(request.form.get("username", None)).strip().lower()
+            password_input = escape(request.form.get("password", None))
+
+            if username_input is None or password_input is None:
+                return render_template("auth/login.html", login_error="Username and/or password are empty")
+
+            row = None
+            connection = db_connect()
+
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT * FROM users WHERE username = %s", (username_input,))
+                    row = cursor.fetchone()
+
+            connection.close()
+
+            if row is None or check_password_hash(row[2], password_input):
+                return render_template("auth/login.html", login_error="Invalid credentials")
+            else:
+                session["username"] = row[1]
+                return redirect("/game")
+
+        else:
+            return render_template("auth/login.html")
+
+    @app.route("/register", methods=["GET", "POST"])
     def register():
-        return render_template('auth/register.html')
+        if session.get("username", -1) != -1:
+            return redirect("/game")
+
+        if request.method == "POST":
+            username_input = escape(request.form.get("username", None)).strip().lower()
+            password_input = escape(request.form.get("password", None))
+            confirm_password_input = escape(request.form.get("confirm_password", None))
+
+            if username_input is None or password_input is None or confirm_password_input is None:
+                return render_template("auth/register.html",
+                                       register_error="Username and/or password and/or confirm password are empty")
+            elif password_input != confirm_password_input:
+                return render_template("auth/register.html", register_error="Passwords do not match")
+
+            row = None
+            connection = db_connect()
+
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT * FROM users WHERE username = %s", (username_input,))
+                    row = cursor.fetchone()
+
+            connection.close()
+
+            if row is not None:
+                return render_template("auth/register.html", register_error="Username already used")
+            else:
+                connection = db_connect()
+
+                with connection:
+                    with connection.cursor() as cursor:
+                        cursor.execute("INSERT INTO users (username,password) VALUES (%s,%s)",
+                                       (username_input, generate_password_hash(password_input)), )
+
+                connection.close()
+
+                session["username"] = username_input
+
+                return redirect("/game")
+        else:
+            return render_template("auth/register.html")
+
+    @app.route("/logout")
+    def logout():
+        session.clear()
+        return redirect("/login")
+
+    @app.route("/game")
+    def game():
+        return render_template("game.html", username=session.get("username"))
 
     return app
